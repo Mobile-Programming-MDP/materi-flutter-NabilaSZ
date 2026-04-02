bf65a583b396717bcc8e5047ac0e4ac35213bfcc
alfitrameddinaramadhani2529250025-debug <nabilasalwazahrani_2226240133@mhs.mdp.ac.id>
2026-04-02 13:10:52 +0700

    update 02

--- cepu_app/cepu_app/lib/screens/home_screen.dart
+++ cepu_app/cepu_app/lib/screens/home_screen.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:cepu_app/screens/sign_up_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cepu_app/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
+    @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+
+  @override Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(child: Text("You Have Been Signed In!"),),
-    );
-  }
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: _showCategoryFilter,
+            icon: const Icon(Icons.filter_list),
+          ),
+          IconButton(
+            onPressed: () {
+              SignUp(context);
+            },
+            icon: const Icon(Icons.logout),
+          )
+        ],
+      ),
 }
